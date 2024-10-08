a83ffcf9ea81280ce3701fb2a65a335e4de657c2
Deyan9469 <deqnpavlikov@gmail.com>
2024-10-08 14:12:00 +0300

    task 7

---
+++ First-steps/src/ProjectsCreation.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ProjectsCreation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String architect = scanner.nextLine();
+        int projects = Integer.parseInt(scanner.nextLine());
+
+        int amountHours = projects * 3;
+
+        System.out.printf("The architect %s will need %d hours to complete %d project/s.", architect, amountHours, projects);
+    }
+}
