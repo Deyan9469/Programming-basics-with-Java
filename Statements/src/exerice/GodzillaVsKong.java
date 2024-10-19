35b378db9e7a426273961258531fc6385027b1a5
Deyan Yosifov <123094953+Deyan9469@users.noreply.github.com>
2024-10-19 16:42:53 +0300

    Task05

---
+++ Statements/src/exerice/GodzillaVsKong.java
@@ -0,0 +1,31 @@
+package exerice;
+
+import java.util.Scanner;
+
+public class GodzillaVsKong {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double budget = Double.parseDouble(scanner.nextLine());
+        int extraCount = Integer.parseInt(scanner.nextLine());
+        double pricePerCloth = Double.parseDouble(scanner.nextLine());
+
+        double decor = budget * 0.10;
+
+        if (extraCount > 150){
+            pricePerCloth = pricePerCloth * 0.90;
+        }
+
+        double finalSum = pricePerCloth * extraCount + decor;
+
+        double difference = Math.abs(finalSum - budget);
+
+        if (finalSum > budget){
+            System.out.println("Not enough money!");
+            System.out.printf("Wingard needs %.2f leva more.", difference);
+        }else {
+            System.out.println("Action!");
+            System.out.printf("Wingard starts filming with %.2f leva left.", difference);
+        }
+    }
+}
