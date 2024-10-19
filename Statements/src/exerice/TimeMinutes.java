f2b09da46aca0493476160317735eba75242d8ed
Deyan Yosifov <123094953+Deyan9469@users.noreply.github.com>
2024-10-19 16:34:38 +0300

    task03

---
+++ Statements/src/exerice/TimeMinutes.java
@@ -0,0 +1,24 @@
+package exerice;
+
+import java.util.Scanner;
+
+public class TimeMinutes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int inputHours = Integer.parseInt(scanner.nextLine());
+        int inputMinutes = Integer.parseInt(scanner.nextLine());
+
+        int totalMinutes = inputHours * 60 + inputMinutes + 15;
+
+        int finalHours = totalMinutes / 60;
+        int finalMinutes = totalMinutes % 60;
+
+        if (finalHours < 24) {
+            System.out.printf("%d:%02d", finalHours, finalMinutes);
+        } else {
+            finalHours = 0;
+            System.out.printf("%d:%02d", finalHours, finalMinutes);
+        }
+    }
+}
