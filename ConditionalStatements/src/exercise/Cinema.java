adb567d26e5b0ae2d398963e5ba946f5e5d713a9
Deyan Yosifov <123094953+Deyan9469@users.noreply.github.com>
2024-10-27 18:22:07 +0200

    Task01

---
+++ ConditionalStatements/src/exercise/Cinema.java
@@ -0,0 +1,24 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class Cinema {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String projection = scanner.nextLine();
+        int rows = Integer.parseInt(scanner.nextLine());
+        int cols = Integer.parseInt(scanner.nextLine());
+        double income = 0;
+
+        if (projection.equals("Premiere")) {
+            income = rows * cols * 12;
+        } else if (projection.equals("Normal")) {
+            income = rows * cols * 7.50;
+        } else if (projection.equals("Discount")) {
+            income = rows * cols * 5.00;
+        }
+
+        System.out.printf("%.2f leva", income);
+    }
+}
